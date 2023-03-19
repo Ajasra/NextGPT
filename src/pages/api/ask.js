ec9ec650be6e5fd9f2123a23e3b99f10d35f20c5
Ajasra <betinvasilij@gmail.com>
2023-03-19 13:05:37 +0800

    - fixing response error

--- src/pages/api/ask.js
+++ src/pages/api/ask.js
@@ -14,61 +14,61 @@ export default async function handler(req, res) {
 
     if (type == "Translate to chinese") {
       msgs.push({
-        role: "system",
-        content:
+        "role": "system",
+        "content":
           "You are a helpful assistant that translates English to Chinese.",
       });
       msgs.push({
-        role: "user",
-        content: `Translate the following English text to Chinese: "${message}"`,
+        "role": "user",
+        "content": `Translate the following English text to Chinese: "${message}"`,
       });
     } else if (type == "Translate to english") {
       msgs.push({
-        role: "system",
-        content:
+        "role": "system",
+        "content":
           "You are a helpful assistant that translates Chinese to English.",
       });
       msgs.push({
-        role: "user",
-        content: `Translate the following Chinese text to English: "${message}"`,
+        "role": "user",
+        "content": `Translate the following Chinese text to English: "${message}"`,
       });
     } else if (type == "Summarize") {
       msgs.push({
-        role: "system",
-        content: "You are a helpful assistant that summarizes text.",
+        "role": "system",
+        "content": "You are a helpful assistant that summarizes text.",
       });
       msgs.push({
-        role: "user",
-        content: `Summarize the following text: "${message}"`,
+        "role": "user",
+        "content": `Summarize the following text: "${message}"`,
       });
     }else if(type == 'Excited') {
       msgs.push({
-        role: "system",
-        content: "You are a very excited and helpful assistant.",
+        "role": "system",
+        "content": "You are a very excited and helpful assistant.",
       });
       msgs.push({
-        role: "user",
-        content: `${message}`,
+        "role": "user",
+        "content": `${message}`,
       });
     }else if(type == 'Lazy') {
         msgs.push({
-            role: "system",
-            content: "You are a very lazy and creative assistant that prefer simple solution.",
+            "role": "system",
+            "content": "You are a very lazy and creative assistant that prefer simple solution.",
         });
         msgs.push({
-            role: "user",
-            content: `${message}`,
+            "role": "user",
+            "content": `${message}`,
         });
     } else {
-      msgs.push({ role: "system", content: "You are helpful, creative, clever, and very friendly assistant." });
+      msgs.push({ "role": "system", "content": "You are helpful, creative, clever, and very friendly assistant." });
       if (chat) {
         values.forEach((value) => {
-          msgs.push({ role: "user", content: value.question });
-          msgs.push({ role: "assistant", content: value.answer });
+          msgs.push({ "role": "user", "content": value.question });
+          msgs.push({ "role": "assistant", "content": value.answer });
         });
       }
 
-      msgs.push({ role: "user", content: message });
+      msgs.push({ "role": "user", "content": message });
     }
 
     // console.log(msgs)
