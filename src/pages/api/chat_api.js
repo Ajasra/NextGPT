b14ff5c15e8a927203d4daba88b0c492108e7a3e
Ajasra <betinvasilij@gmail.com>
2023-03-28 15:26:46 +0800

    - backend changed to opanai library

--- src/pages/api/chat_api.js
+++ src/pages/api/chat_api.js
@@ -1,16 +1,17 @@
-const axios = require("axios");
+const { Configuration, OpenAIApi } = require("openai");
 
-const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
 const LOCAL_KEY = process.env.NEXT_PUBLIC_LOCAL_KEY;
+
+const configuration = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     
     let messages = req.body.messages;
     let key = req.body.key;
-    
-    // console.log(messages);
 
     if (key !== LOCAL_KEY) {
       res.status(404).json({ error: "Access denied" });
@@ -21,31 +22,18 @@ export default async function handler(req, res) {
       res.status(404).json({ error: "Empty message" });
       return;
     }
-
-    await axios
-      .post(
-        API_ENDPOINT,
-        {
-          model: "gpt-3.5-turbo",
-          stream: false,
-          temperature: 0.3,
-          messages: messages,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            Authorization: `Bearer ${OPENAI_API_KEY}`,
-          },
-        }
-      )
-      .then((response) => {
-        const answer = response.data;
-        res.status(200).json({ error: null, response: answer });
-      })
-      .catch((error) => {
-        res.status(500).json({ error: error });
-      });
+  
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      stream: false,
+        temperature: 0.3,
+      messages: messages,
+    }).then((response) => {
+      const answer = response.data;
+      res.status(200).json({ error: null, response: answer });
+    }).catch((error) => {
+      res.status(500).json({ error: error });
+    });
     
   } else {
     res.status(404).json({ error: "Not found" });
